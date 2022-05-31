2deb203b3ae0dda1a88e18f571fab63f32a8d3a2
Yann <tricot.yann@gmail.com>
2022-05-31 23:11:25 +0200

    feat: Add Yann's features

--- src/main/java/com/example/travellingsalesmanv3/Model/Algos/RecuitSimule.java
+++ src/main/java/com/example/travellingsalesmanv3/Model/Algos/RecuitSimule.java
@@ -4,8 +4,11 @@ import com.example.travellingsalesmanv3.Model.Structure.Map;
 import com.example.travellingsalesmanv3.Model.Tools.Tools;
 import com.example.travellingsalesmanv3.Model.TransfoElementaire.VoisinAlgo;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class RecuitSimule extends Algorithme {
@@ -20,14 +23,12 @@ public class RecuitSimule extends Algorithme {
 
     @Override
     public Map lancer(Map map) {
-
         map = this.recuitSimule(map);
         return map;
     }
 
 
     private Map recuitSimule(Map map) {
-
         long startTime = System.nanoTime();
         Random rnd = new Random();
         Map cloneMap = map.cloneMap();
@@ -36,49 +37,51 @@ public class RecuitSimule extends Algorithme {
         double minFitness = Integer.MAX_VALUE;
         double fitnessBestSolution = Tools.calculerDistanceTotal(cloneMap);
         Map nextVoisin = null;
-        for (int k = 0; k < N1; k++) {
-            for (int l = 1; l < N2; l++) {
-                ArrayList<Map> voisin = new ArrayList<>();
-                for (VoisinAlgo voisinAlgo : this.voisins) {
-                    voisin.addAll(voisinAlgo.lancerToutVoisin(cloneMap));
-                }
-                //choix du voisin random
-                Map randomVoisin = voisin.get(rnd.nextInt(voisin.size() - 1));
-                double fitnessVoisin = Tools.calculerDistanceTotal(randomVoisin);
 
-                var diffFitness = fitnessVoisin - fitnessBestSolution;
+        try {
+            FileWriter myWriter = new FileWriter("src/main/java/com/example/travellingsalesmanv3/Model/Results/RECUIT_" + UUID.randomUUID() + ".txt");
+            for (int k = 0; k < N1; k++) {
+                for (int l = 1; l < N2; l++) {
+                    ArrayList<Map> voisin = new ArrayList<>();
+                    for (VoisinAlgo voisinAlgo : this.voisins)
+                        voisin.addAll(voisinAlgo.lancerToutVoisin(cloneMap));
 
+                    //choix du voisin random
+                    Map randomVoisin = voisin.get(rnd.nextInt(voisin.size() - 1));
+                    double fitnessVoisin = Tools.calculerDistanceTotal(randomVoisin);
 
-                //si voisin est meilleur
-                if (diffFitness <= 0) {
-                    nextVoisin = randomVoisin;
-                    if (fitnessVoisin < minFitness) {
-                        fitnessBestSolution = fitnessVoisin;
-                        bestSolution = randomVoisin;
-                    }
-                } else {
-                    double proba = rnd.nextDouble();
-                    var a = Math.exp(-diffFitness / temperature);
-                    if (proba <= Math.exp(-diffFitness / temperature)) {
+                    var diffFitness = fitnessVoisin - fitnessBestSolution;
+
+                    //si voisin est meilleur
+                    if (diffFitness <= 0) {
                         nextVoisin = randomVoisin;
+                        if (fitnessVoisin < minFitness) {
+                            fitnessBestSolution = fitnessVoisin;
+                            bestSolution = randomVoisin;
+                        }
+                    } else {
+                        double proba = rnd.nextDouble();
+                        var a = Math.exp(-diffFitness / temperature);
+                        if (proba <= Math.exp(-diffFitness / temperature)) {
+                            nextVoisin = randomVoisin;
+                        } else { nextVoisin = cloneMap; }
                     }
-                    else
-                    {
-                        nextVoisin = cloneMap;
-                    }
+                    cloneMap = nextVoisin;
+                    myWriter.write(Double.toString(Tools.calculerDistanceTotal(cloneMap)) + "\n");
 
                 }
-                cloneMap = nextVoisin;
-
+                temperature = mu * temperature;
+    //            long locElapsedTime = System.nanoTime() - startTime;
+    //            long locDurationInMs = TimeUnit.MILLISECONDS.convert(locElapsedTime, TimeUnit.NANOSECONDS);
+    //            System.out.println("Exec time: " + locDurationInMs + "ms");
             }
-            temperature = mu * temperature;
-//            long locElapsedTime = System.nanoTime() - startTime;
-//            long locDurationInMs = TimeUnit.MILLISECONDS.convert(locElapsedTime, TimeUnit.NANOSECONDS);
-//            System.out.println("Exec time: " + locDurationInMs + "ms");
-        }
-        long elapsedTime = System.nanoTime() - startTime;
-        long durationInMs = TimeUnit.MILLISECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS);
-        System.out.println("Total exec. time: " + durationInMs + "ms");
+            long elapsedTime = System.nanoTime() - startTime;
+            long durationInMs = TimeUnit.MILLISECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS);
+            System.out.println("Total exec. time: " + durationInMs + "ms");
+
+            myWriter.write(Long.toString(durationInMs) + "\n");
+            myWriter.close();
+        }catch (IOException e){}
         return bestSolution;
     }
 }
