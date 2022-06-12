0b76f434da1fbe28d6ab9e444558a1698b8c1ba0
Alexandre <alex.chevreaud@icloud.com>
2022-06-12 16:59:36 +0200

    amelioration temps tabou

--- src/main/java/com/example/travellingsalesmanv3/Model/Algos/Tabu.java
+++ src/main/java/com/example/travellingsalesmanv3/Model/Algos/Tabu.java
@@ -4,11 +4,7 @@ import com.example.travellingsalesmanv3.Model.Structure.Map;
 import com.example.travellingsalesmanv3.Model.Tools.Tools;
 import com.example.travellingsalesmanv3.Model.TransfoElementaire.VoisinAlgo;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class Tabu extends Algorithme {
@@ -21,26 +17,25 @@ public class Tabu extends Algorithme {
     }
 
     @Override
-    public Map lancer(Map map,String fileName) {
-        map = this.rechercheTabu(map,fileName);
+    public Map lancer(Map map, String fileName) {
+        map = this.rechercheTabu(map, fileName);
         return map;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "TABU";
     }
 
-    private Map rechercheTabu(Map map,String fileName) {
+    private Map rechercheTabu(Map map, String fileName) {
         TABUCONDITION = true;
         var listToWrite = new ArrayList<Double>();
         int nbSolution = 0;
         int count = 0;
         Map bestSolution = map.cloneMap();
         Map bestCandidat;
-        ArrayList<Map> tabuclients = new ArrayList<>();
-        tabuclients.add(map);
+        ArrayList<Double> tabuclients = new ArrayList<>();
+        tabuclients.add(Tools.calculerDistanceTotal(map));
         while (TABUCONDITION) {
             count++;
             ArrayList<Map> voisin = new ArrayList<>();
@@ -49,35 +44,32 @@ public class Tabu extends Algorithme {
 
             nbSolution += voisin.size();
 
-            bestCandidat = voisin.stream().filter(v -> !tabuclients.contains(v)).collect(Collectors.toList()).stream().findAny().orElse(null);
+            bestCandidat = voisin.stream().filter(v -> !tabuclients.contains(Tools.calculerDistanceTotal(v))).collect(Collectors.toList()).stream().findAny().orElse(null);
 
             if (bestCandidat == null) {
                 TABUCONDITION = false;
                 bestCandidat = bestSolution;
             }
             for (Map sCand : voisin)
-                if (!tabuclients.contains(sCand) && Tools.calculerDistanceTotal(sCand) < Tools.calculerDistanceTotal(bestCandidat))
+                if (!tabuclients.contains(Tools.calculerDistanceTotal(sCand)) && Tools.calculerDistanceTotal(sCand) < Tools.calculerDistanceTotal(bestCandidat))
                     bestCandidat = sCand;
 
-            var d = Tools.calculerDistanceTotal(bestCandidat);
+            Double d = Tools.calculerDistanceTotal(bestCandidat);
             listToWrite.add(d);
 
-            if (Tools.calculerDistanceTotal(bestCandidat) < Tools.calculerDistanceTotal(bestSolution))
-            {
+            if (Tools.calculerDistanceTotal(bestCandidat) < Tools.calculerDistanceTotal(bestSolution)) {
                 bestSolution = bestCandidat;
             }
 
-            tabuclients.add(bestCandidat);
-            if (count%1000==0)
-                System.out.println(count);
+            tabuclients.add(Tools.calculerDistanceTotal(bestCandidat));
+            if (count % 100 == 0)
+                System.out.println(nbSolution);
 
         }
 
 
-        super.WriteToFile(fileName,listToWrite);
-        System.out.println("Nombre de solutiosn générées : "+nbSolution);
-
-
+        super.WriteToFile(fileName, listToWrite);
+        System.out.println("Nombre de solutiosn générées : " + nbSolution);
 
 
         return bestSolution;
